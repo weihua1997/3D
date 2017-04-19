6bd06bcd48250312bceb6255cf6d2e8d96679715
weihua1997 <a7510981@gmail.com>
2017-04-19 23:25:10 +0800

    walk

---
+++ walk/Assets/Walk.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : MonoBehaviour {
+    Animator anim;
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("up")) {
+            anim.SetBool("next",true);
+        }
+	}
+}
